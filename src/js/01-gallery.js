32079367cc4724dbcda8bd92534bc7749a81da2d
VasylArtymovych <art777vasss@gmail.com>
2022-04-12 20:17:19 +0300

    add task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function createGalleryMurkup(obj) {
+    const gallery = obj.map(createGalleryElement).join('');
+
+    galleryRef.innerHTML = gallery;
+}
+createGalleryMurkup(galleryItems);
+
+function createGalleryElement({preview, original, description}) {
+    return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a>`
+};
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+gallery.on(() => {
+    gallery.next();
+});
